547342acdfd944a407a4cf340671defdd608f81c
tom <shuaifeng12138@gmail.com>
2020-08-28 16:54:32 +0800

    使用umi3进行页面初始化

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.less';
+
+export default () => {
+  return (
+    <div>
+      <h1 className={styles.title}>滴答医疗1.0.0 爷来啦</h1>
+    </div>
+  );
+};
