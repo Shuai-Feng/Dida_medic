25ae380c5dbb0fa9313209ad5524a2560aba7e39
tom <shuaifeng12138@gmail.com>
2020-08-28 15:16:26 +0800

    原来的都不要了 直接重构

--- data/conn.php
+++
@@ -1,6 +0,0 @@
-<?php
-        // @header("Content-Type:text/html;charset=utf-8");
-        $conn=mysql_connect("localhost","root","") or die("wrong");
-        mysql_select_db("medic",$conn);
-        mysql_query("set names utf8");
-?>
